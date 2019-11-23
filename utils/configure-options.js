9dd4f6f108442dc247de44e1ddc0341edcb84c9b
Michael Perrotte <mike@npmjs.com>
2019-11-23 14:07:18 -0500

    feat(utils): fixed configure-options based on testing

--- utils/configure-options.js
+++ utils/configure-options.js
@@ -6,22 +6,23 @@ module.exports = function configureOptions (_opts) {
   const opts = Object.assign({}, _opts || {})
   opts.method = (opts.method || 'GET').toUpperCase()
 
-  if (opts.retry) {
-    if (typeof opts.retry === 'number') {
-      opts.retry = { retries: opts.retry }
-    } else if (typeof opts.retry === 'string') {
-      const value = parseInt(opts.retry, 10)
-      if (value) {
-        opts.retry = { retries: value }
-      } else {
-        opts.retry = { retries: 0 }
-      }
-    } else {
-      opts.retry = { retries: 0 }
-    }
-  } else {
+  if (!opts.retry) {
     // opts.retry was falsy; set default
     opts.retry = { retries: 0 }
+  } else {
+    if (typeof opts.retry !== 'object') {
+      // Shorthand
+      if (typeof opts.retry === 'number') {
+        opts.retry = { retries: opts.retry }
+      }
+      if (typeof opts.retry === 'string') {
+        const value = parseInt(opts.retry, 10)
+        opts.retry = (value) ? { retries: value } : { retries: 0 }
+      }
+    } else {
+      // Set default retries
+      opts.retry = Object.assign({}, { retries: 0 }, opts.retry)
+    }
   }
 
   if (opts.cacheManager) {
