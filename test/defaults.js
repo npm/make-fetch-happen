e0d86998f7583cc83b5f43a94962374d290dc6c3
David Glasser <glasser@davidglasser.net>
2022-04-29 10:48:55 -0700

    fix: make `defaults` chaining actually work
    The README says:
    
    > A defaulted `fetch` will also have a `.defaults()` method, so they can
    > be chained.
    
    But this didn't really work, because while the `defaults` method was
    placed on the `defaulted` fetch, a chained defaulted fetch would just
    call the original fetch function directly instead of the intermediate
    function.
    
    This PR makes chaining actually work and adds a test.
    
    Also, it fixes the previous test, which wasn't actually verifying that
    the correct request headers were received because it used the nock API
    incorrectly: the option is `reqheaders` not `reqHeaders`.

--- test/defaults.js
+++ test/defaults.js
@@ -21,7 +21,7 @@ t.test('can set default url', async (t) => {
 
 t.test('allows default headers', async (t) => {
   const srv = nock('http://localhost', {
-    reqHeaders: {
+    reqheaders: {
       'x-foo': 'bar',
       'x-bar': 'baz',
     },
@@ -33,6 +33,30 @@ t.test('allows default headers', async (t) => {
   const res = await defaultedFetch('http://localhost/test', {
     headers: {
       'x-bar': 'baz',
+    },
+  })
+  t.equal(res.status, 200, 'got success')
+  const buf = await res.buffer()
+  t.same(buf, Buffer.from('success'), 'got body')
+  t.ok(srv.isDone())
+})
+
+t.test('layering default headers works', async (t) => {
+  const srv = nock('http://localhost', {
+    reqheaders: {
+      'x-foo': 'bar',
+      'x-bar': 'baz',
+      'x-another': 'yep',
+    },
+  })
+    .get('/test')
+    .reply(200, 'success')
+
+  const defaultedFetch1 = fetch.defaults({ headers: { 'x-foo': 'bar' } })
+  const defaultedFetch2 = defaultedFetch1.defaults({ headers: { 'x-bar': 'baz' } })
+  const res = await defaultedFetch2('http://localhost/test', {
+    headers: {
+      'x-another': 'yep',
     },
   })
   t.equal(res.status, 200, 'got success')
